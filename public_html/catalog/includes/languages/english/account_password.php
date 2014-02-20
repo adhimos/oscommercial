e4b3cbac3f778fa8edaf259191e3e323734b0b37
student <student@osCommerce.(none)>
2014-02-20 23:18:42 +0800

    FIX Password Change email

--- public_html/catalog/includes/languages/english/account_password.php
+++ public_html/catalog/includes/languages/english/account_password.php
@@ -18,5 +18,7 @@ define('HEADING_TITLE', 'My Password');
 define('MY_PASSWORD_TITLE', 'My Password');
 
 define('SUCCESS_PASSWORD_UPDATED', 'Your password has been successfully updated.');
+define('EMAIL_PASSWORD_RESET_SUBJECT', STORE_NAME . ' - New Password');
+define('EMAIL_PASSWORD_RESET_BODY', 'A new password has been updated for your account at ' . STORE_NAME . '.' . "\n\n" );
 define('ERROR_CURRENT_PASSWORD_NOT_MATCHING', 'Your Current Password did not match the password in our records. Please try again.');
 ?>
